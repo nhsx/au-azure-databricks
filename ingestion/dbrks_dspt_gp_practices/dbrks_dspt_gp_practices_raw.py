48ac326f5887410bce05635314f9fac9d1851850
mattia.ficarelli@nhsx.nhs.uk <mattia.ficarelli@nhsx.nhs.uk>
2022-01-19 17:43:07 +0000

    dev_dspt_gp_ingestions
    @faaiz-25 I cleaned up the notebook

---
+++ ingestion/dbrks_dspt_gp_practices/dbrks_dspt_gp_practices_raw.py
@@ -0,0 +1,125 @@
+# Databricks notebook source
+#!/usr/bin python3
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2022 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:           dbrks_dspt_gp_practices_raw.py
+DESCRIPTION:
+                Databricks notebook with code to append new raw data to historical
+                data for the NHSX Analytics unit metrics within the topic DSPT gp practices 
+USAGE:
+                ...
+CONTRIBUTORS:   Craig Shenton, Mattia Ficarelli, Muhammad-Faaiz Shanawas
+CONTACT:        data@nhsx.nhs.uk
+CREATED:        19 Jan. 2022
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.* xlrd openpyxl python-dateutil
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+import requests
+from pathlib import Path
+from urllib import request as urlreq
+from bs4 import BeautifulSoup
+from azure.storage.filedatalake import DataLakeServiceClient
+from dateutil.relativedelta import relativedelta
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Repos/dev/au-azure-databricks/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+# Load JSON config from Azure datalake
+# -------------------------------------------------------------------------
+#file_path_config = "/config/pipelines/nhsx-au-analytics/"
+#file_name_config = "config_dspt_socialcare_dbrks.json"
+#file_system_config = "nhsxdatalakesagen2fsprod"
+#config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+#config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+# Read parameters from JSON config
+# -------------------------------------------------------------------------
+file_system = 'nhsxdatalakesagen2fsprod'
+new_source_path = 'land/nhsdigital/manual_upload/timestamp/csv/dspt_gp_practices/'
+historical_source_path = 'raw/nhsdigital/manual_upload/parquet/dspt_gp_practice/historical/'
+historical_source_file = 'dspt_gp_practice_historical.parquet'
+sink_path = 'raw/nhsdigital/manual_upload/parquet/dspt_gp_practice/historical/'
+sink_file = 'dspt_gp_practice_historical.parquet'
+
+# COMMAND ----------
+
+# Pull new snapshot dataset
+# -------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, new_source_path)
+file_name_list = datalake_listContents(CONNECTION_STRING, file_system, new_source_path+latestFolder)
+file_name_list = [file for file in file_name_list if '.csv' in file]
+for new_source_file in file_name_list:
+  new_dataset = datalake_download(CONNECTION_STRING, file_system, new_source_path+latestFolder, new_source_file)
+  new_dataframe = pd.read_csv(io.BytesIO(new_dataset))
+
+# COMMAND ----------
+
+# Alter new snapshot dataset
+# --------------------------
+year = get_year_dspt_gp(latestFolder)
+df_processed = process_dspt_dataframe(new_dataframe, new_source_file, year)
+
+# COMMAND ----------
+
+# Pull historical dataset
+# -----------------------------------------------------------------------
+latestFolder_historical = datalake_latestFolder(CONNECTION_STRING, file_system, historical_source_path)
+historical_dataset = datalake_download(CONNECTION_STRING, file_system, historical_source_path+latestFolder_historical, historical_source_file)
+historical_dataframe = pd.read_parquet(io.BytesIO(historical_dataset), engine="pyarrow")
+
+# Append new data to historical data
+# -----------------------------------------------------------------------
+dspt_editions_in_historical = historical_dataframe["DSPT_Edition"].unique().tolist()
+dspt_editions_in_new = df_processed["DSPT_Edition"].unique().tolist()[0]
+if dspt_editions_in_new in dspt_editions_in_historical:
+  print('Data already exists in historical data')
+else:
+  historical_dataframe = historical_dataframe.append(df_processed)
+  historical_dataframe = historical_dataframe.sort_values(by=['Snapshot_Date'])
+  historical_dataframe = historical_dataframe.reset_index(drop=True)
+  historical_dataframe = historical_dataframe.astype(str)
+
+# COMMAND ----------
+
+#Upload hsitorical appended data to datalake
+# -----------------------------------------------------------------------
+current_date_path = datetime.now().strftime('%Y-%m-%d') + '/'
+file_contents = io.BytesIO()
+historical_dataframe.to_parquet(file_contents, engine="pyarrow")
+datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+current_date_path, sink_file)
