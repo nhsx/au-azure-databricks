0afbf8d410231ecbe5851a51f280971a16d6290e
mattia.ficarelli@nhsx.nhs.uk <mattia.ficarelli@nhsx.nhs.uk>
2022-02-02 10:57:47 +0000

    AI Lab solutions

--- analytics/dbrks_ailab/dbrks_ai_trusts_ailab_solutions_month_count_prop.py
+++ analytics/dbrks_ailab/dbrks_ai_trusts_ailab_solutions_month_count_prop.py
@@ -1,5 +1,4 @@
 # Databricks notebook source
-
 # -------------------------------------------------------------------------
 # Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
 # Licensed under the MIT License. See license.txt in the project root for
@@ -9,7 +8,8 @@
 """
 FILE:          dbrks_ai_trusts_ailab_solutions_month_count_prop.py
 DESCRIPTION:
-                Databricks notebook with processing code for the NHSX Analyticus unit metric: Number and percent of Trusts with at least one NHS AI Lab solution trialled (AI Lab) (M022_M023)
+                
+               Databricks notebook with processing code for the NHSX Analyticus unit metric: Number and percent of Trusts with at least one NHS AI Lab solution trialled (AI Lab) (M022_M023)
 USAGE:
                 ...
 CONTRIBUTORS:   Craig Shenton, Mattia Ficarelli ,Everistus Oputa
@@ -64,11 +64,9 @@ config_JSON = json.loads(io.BytesIO(config_JSON).read())
 
 # Read parameters from JSON config
 # -------------------------------------------------------------------------
+file_system = config_JSON['pipeline']['adl_file_system']
 source_path = config_JSON['pipeline']['project']['source_path']
 source_file = config_JSON['pipeline']['project']['source_file']
-reference_path = config_JSON['pipeline']['project']['reference_path']
-reference_file = config_JSON['pipeline']['project']['reference_file']
-file_system = config_JSON['pipeline']['adl_file_system']
 sink_path = config_JSON['pipeline']['project']['sink_path']
 sink_file = config_JSON['pipeline']['project']['sink_file']  
 
@@ -77,61 +75,12 @@ sink_file = config_JSON['pipeline']['project']['sink_file']
 # Processing
 # -------------------------------------------------------------------------
 latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, source_path)
-reference_latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, reference_path)
-
 file = datalake_download(CONNECTION_STRING, file_system, source_path+latestFolder, source_file)
-reference_file = datalake_download(CONNECTION_STRING, file_system, reference_path+reference_latestFolder, reference_file)
-
-
 df = pd.read_csv(io.BytesIO(file))
-ODS_code_df = pd.read_parquet(io.BytesIO(reference_file), engine="pyarrow")
-
-df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-df1 = df[["Organisation_Code", "Trust_PilotFlag", "Trust_Live", "Last_Refreshed"]]
-ailab_ODS = pd.merge(df1, ODS_code_df, left_on="Organisation_Code", right_on="Code")
-ailab_ODS = (ailab_ODS.drop(
-            [
-                "Address_Line_1",
-                "Address_Line_2",
-                "Address_Line_3",
-                "Address_Line_4",
-                "Address_Line_5",
-                "Postcode",
-                "Open_Date",
-                "Close_Date",
-                "Import_Date",
-                "Created_Date",
-            ],
-            1,
-        )
-        .reset_index(drop=True)
-        .rename(
-            columns={
-                "Date Of Publication": "Date Of DSPT Publication",
-            }
-        )
-    )
-df2 = ailab_ODS[["Trust_PilotFlag", "Trust_Live", "Last_Refreshed"]]
-df2 = df2.reset_index(drop=True)
-No_Trusts_AI = df2["Trust_PilotFlag"].sum()
-Total_Trusts = df2["Trust_Live"].sum()
-Percent_Trusts_AI = No_Trusts_AI / Total_Trusts
-Date = df2["Last_Refreshed"].max()
-
-Data_f = [[No_Trusts_AI, Total_Trusts, Percent_Trusts_AI, Date]]
-df3 = pd.DataFrame(
-        Data_f,
-        columns=[
-            "Number of Trusts with at least one NHS AI Lab solution trialled",
-            "Total number of Trusts",
-            "Percent of Trusts with at least one NHS AI Lab solution trialled",
-            "Data updated",
-        ],
-    )
-df3 = df3.round(4)
-df3 = df3.reset_index(drop=True)
-df3.index.name = "Unique ID"
-df_processed = df3.copy()
+df['Percent of Number of Trusts with at least one NHS AI Lab solution trialled'] = df['Number of Trusts (England) with at least one NHS AI Lab solution trialled']/df['Total number of Trusts (England)']
+df_1 = df.rename(columns = {'Number of Trusts (England) with at least one NHS AI Lab solution trialled': 'Number of Trusts with at least one NHS AI Lab solution trialled', 
+                            "Total number of Trusts (England)": "Total number of Trusts"})
+df_processed = df_1.copy()
 
 # COMMAND ----------
 
---
+++ ingestion/dbrks_ailab/dbrks_ailab_solutions_raw.py
@@ -0,0 +1,118 @@
+# Databricks notebook source
+#!/usr/bin python3
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:           dbrks_ailab_solutions_raw.py
+DESCRIPTION:
+                Databricks notebook with code to append new raw data to historical
+                data for the NHSX Analytics unit metrics within the topic AI lab solutions trialled at Trusts
+USAGE:
+                ...
+CONTRIBUTORS:   Mattia Ficarelli
+CONTACT:        data@nhsx.nhs.uk
+CREATED:        02 Feb. 2022
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.* xlrd openpyxl python-dateutil
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+import requests
+from pathlib import Path
+from urllib import request as urlreq
+from bs4 import BeautifulSoup
+from azure.storage.filedatalake import DataLakeServiceClient
+from dateutil.relativedelta import relativedelta
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Repos/dev/au-azure-databricks/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+# Load JSON config from Azure datalake
+# -------------------------------------------------------------------------
+file_path_config = "/config/pipelines/nhsx-au-analytics/"
+file_name_config = "config_ailab_solutions_dbrks.json"
+file_system_config = "nhsxdatalakesagen2fsprod"
+config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+# Read parameters from JSON config
+# -------------------------------------------------------------------------
+file_system = config_JSON['pipeline']['adl_file_system']
+new_source_path = config_JSON['pipeline']['raw']['snapshot_source_path']
+historical_source_path = config_JSON['pipeline']['raw']['appended_path']
+historical_source_file = config_JSON['pipeline']['raw']['appended_file']
+sink_path = config_JSON['pipeline']['raw']['appended_path']
+sink_file = config_JSON['pipeline']['raw']['appended_file']
+
+# COMMAND ----------
+
+# Pull new snapshot dataset
+# -------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, new_source_path)
+file_name_list = datalake_listContents(CONNECTION_STRING, file_system, new_source_path+latestFolder)
+file_name_list = [file for file in file_name_list if '.xlsx' in file]
+for new_source_file in file_name_list:
+  new_dataset = datalake_download(CONNECTION_STRING, file_system, new_source_path+latestFolder, new_source_file)
+  new_dataframe = pd.read_excel(io.BytesIO(new_dataset), sheet_name = 'NHSX Analytic Unit Data', engine  = 'openpyxl') #------ check this is true when email dataflow starts
+  new_dataframe['Date'] = pd.to_datetime(new_dataframe['Date']).dt.strftime('%Y-%m-%d')
+
+# COMMAND ----------
+
+# Pull historical dataset
+# -----------------------------------------------------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, historical_source_path)
+historical_dataset = datalake_download(CONNECTION_STRING, file_system, historical_source_path+latestFolder, historical_source_file)
+historical_dataframe = pd.read_csv(io.BytesIO(historical_dataset))
+historical_dataframe['Date'] = pd.to_datetime(historical_dataframe['Date']).dt.strftime('%Y-%m-%d')
+
+# Append new data to historical data
+# -----------------------------------------------------------------------
+dates_in_historical = historical_dataframe["Date"].unique().tolist()
+dates_in_new = new_dataframe["Date"].unique().tolist()[0]
+if dates_in_new in dates_in_historical:
+  print('Data already exists in historical data')
+else:
+  historical_dataframe = historical_dataframe.append(new_dataframe )
+  historical_dataframe = historical_dataframe.sort_values(by=['Date'])
+  historical_dataframe = historical_dataframe.reset_index(drop=True)
+
+# COMMAND ----------
+
+# Upload hsitorical appended data to datalake
+current_date_path = datetime.now().strftime('%Y-%m-%d') + '/'
+file_contents = io.StringIO()
+historical_dataframe.to_csv(file_contents, index=False)
+datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+current_date_path, sink_file)
