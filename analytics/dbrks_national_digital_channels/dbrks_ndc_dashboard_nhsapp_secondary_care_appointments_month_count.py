b9b8f7cac1e2b49a5eba1e31720e548f2759a8a6
mattia.ficarelli@nhsx.nhs.uk <mattia.ficarelli@nhsx.nhs.uk>
2022-06-13 16:28:05 +0000

    NDC Beta Analytics Notebooks
    M225
    M226
    M229
    M231
    M232

---
+++ analytics/dbrks_national_digital_channels/dbrks_ndc_dashboard_nhsapp_secondary_care_appointments_month_count.py
@@ -0,0 +1,99 @@
+# Databricks notebook source
+#!/usr/bin python3
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2021 NHS England and NHS Improvement. All rights reserved.
+# Licensed under the MIT License. See license.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+"""
+FILE:           dbrks_ndc_dashboard_nhsapp_secondary_care_appointments_month_count.py
+DESCRIPTION:
+                Databricks notebook with processing code for the NHSX Analyticus unit metric M226: Secondary care appointments via NHS App
+USAGE:
+                ...
+CONTRIBUTORS:   Mattia Ficarelli
+CONTACT:        data@nhsx.nhs.uk
+CREATED:        13th June 2022
+VERSION:        0.0.1
+"""
+
+# COMMAND ----------
+
+# Install libs
+# -------------------------------------------------------------------------
+%pip install geojson==2.5.* tabulate requests pandas pathlib azure-storage-file-datalake beautifulsoup4 numpy urllib3 lxml regex pyarrow==5.0.*
+
+# COMMAND ----------
+
+# Imports
+# -------------------------------------------------------------------------
+# Python:
+import os
+import io
+import tempfile
+from datetime import datetime
+import json
+
+# 3rd party:
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from azure.storage.filedatalake import DataLakeServiceClient
+
+# Connect to Azure datalake
+# -------------------------------------------------------------------------
+# !env from databricks secrets
+CONNECTION_STRING = dbutils.secrets.get(scope="datalakefs", key="CONNECTION_STRING")
+
+# COMMAND ----------
+
+# MAGIC %run /Repos/prod/au-azure-databricks/functions/dbrks_helper_functions
+
+# COMMAND ----------
+
+#Download JSON config from Azure datalake
+file_path_config = "/config/pipelines/nhsx-au-analytics/"
+file_name_config = "config_national_digital_channels_dbrks.json"
+file_system_config = "nhsxdatalakesagen2fsprod"
+config_JSON = datalake_download(CONNECTION_STRING, file_system_config, file_path_config, file_name_config)
+config_JSON = json.loads(io.BytesIO(config_JSON).read())
+
+# COMMAND ----------
+
+#Get parameters from JSON config
+file_system = config_JSON['pipeline']['adl_file_system']
+source_path = config_JSON['pipeline']['project']['source_path']
+source_file = config_JSON['pipeline']['project']["source_file_monthly"]
+sink_path = config_JSON['pipeline']['project']['databricks'][21]['sink_path']
+sink_file = config_JSON['pipeline']['project']['databricks'][21]['sink_file']  
+
+# COMMAND ----------
+
+# Ingestion of numerator data (NHS app performance data)
+# ---------------------------------------------------------------------------------------------------
+latestFolder = datalake_latestFolder(CONNECTION_STRING, file_system, source_path)
+file = datalake_download(CONNECTION_STRING, file_system, source_path+latestFolder, source_file)
+df = pd.read_parquet(io.BytesIO(file), engine="pyarrow")
+
+# COMMAND ----------
+
+#Processing
+# ---------------------------------------------------------------------------------------------------
+
+#Numerator
+# ---------------------------------------------------------------------------------------------------
+df_1 = df[["Monthly", "PKB_appointments"]]
+df_1.iloc[:, 0] = df_1.iloc[:,0].dt.strftime('%Y-%m')
+df_2 = df_1.groupby(df_1.iloc[:,0]).sum().reset_index()
+df_2.rename(columns  = {'Monthly': 'Date', "PKB_appointments": 'Number of secondary care appointments made via the NHS App'}, inplace = True)
+df_2.index.name = "Unique ID"
+df_processed = df_2.copy()
+
+# COMMAND ----------
+
+#Upload processed data to datalake
+file_contents = io.StringIO()
+df_processed.to_csv(file_contents)
+datalake_upload(file_contents, CONNECTION_STRING, file_system, sink_path+latestFolder, sink_file)
